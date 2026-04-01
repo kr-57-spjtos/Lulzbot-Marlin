041f0ac32ea25c7e72edb776b1df238ba4e46a47
kr-57-spjtos <kr57@illinois.edu>
2026-04-01 07:54:12 -0500

    Made TEMP_SENSOR_0 and TEMP_SENSOR_1 unused

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -743,14 +743,14 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 5
-#define TEMP_SENSOR_1 LULZBOT_TEMP_SENSOR_1
+#define TEMP_SENSOR_0 0
+#define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_5 0
 #define TEMP_SENSOR_6 0
-#define TEMP_SENSOR_7 0
+#define TEMP_SENSOR_7 0 
 #define TEMP_SENSOR_BED 7
 #define TEMP_SENSOR_PROBE 0
 #define TEMP_SENSOR_CHAMBER 0
