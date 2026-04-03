2a8bb8fe3df7b75f549567743990688538d73f5b
kr-57-spjtos <kr57@illinois.edu>
2026-04-02 21:00:23 -0500

    Edited max/endstop coordinates to account for longer bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1952,15 +1952,15 @@
     #define LULZBOT_Z_MIN_POS -9 // <-- changed
     #define LULZBOT_Z_MAX_POS 299 // <-- changed 
   #else
-    #define X_BED_SIZE 284
-    #define Y_BED_SIZE 286
+    #define X_BED_SIZE 282
+    #define Y_BED_SIZE 582
     // Travel limits (mm) after homing, corresponding to endstop positions.
     #define LULZBOT_X_MIN_POS -1
     #define LULZBOT_Y_MIN_POS -9
-    #define LULZBOT_X_MAX_POS 313
-    #define LULZBOT_Y_MAX_POS 318
+    #define LULZBOT_X_MAX_POS 289
+    #define LULZBOT_Y_MAX_POS 613
     #define LULZBOT_Z_MIN_POS -9
-    #define LULZBOT_Z_MAX_POS 299 
+    #define LULZBOT_Z_MAX_POS 285 
   #endif
 #elif ENABLED(TAZProXT)
     #if defined(TOOLHEAD_Quiver_DualExtruder)
